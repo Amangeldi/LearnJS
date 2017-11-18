5e06776b470a6605eb9f86470f7bc41177256711
Amangeldi <Amangeldi.9606@gmail.com>
2017-11-18 22:58:23 +0500

    Проверка логина

--- alert.js
+++ alert.js
@@ -1,1 +1,25 @@
-var test = prompt("Тест");
+var test = prompt('Кто пришел?', '');
+if(test==null)
+{
+	alert("Вход отменен");
+}
+else if(test=="Админ")
+{
+	var pass = prompt('Пароль?', '');
+	if(pass==null)
+	{
+		alert("Вход отменен");
+	}
+	else if(pass=="Чёрный Властелин")
+	{
+		alert("Добро пожаловать!");
+	}
+	else
+	{
+		alert("Пароль неверен");
+	}
+}
+else
+{
+	alert("Я вас не знаю");
+}
