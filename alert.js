eaa6a7099d3f62cceb311ff204e2e5f0b9593c6b
Amangeldi <Amangeldi.9606@gmail.com>
2017-11-21 15:01:34 +0500

    alert( 0xFF );

--- alert.js
+++ alert.js
@@ -1,15 +1,1 @@
-function factorial(n)
-{
-	if(n==1)
-	{
-		return 1;
-	}
-	else
-	{
-		return n*factorial(n-1);
-	}
-}
-
-alert(factorial(3));
-alert(factorial(4));
-alert(factorial(5));
+alert( 0xFF );
