2d683cfd8b98c960366df7035c75519012225974
Amangeldi <Amangeldi.9606@gmail.com>
2017-11-21 16:15:05 +0500

    дробная часть числа

--- alert.js
+++ alert.js
@@ -1,5 +1,8 @@
-var i = 0;
-while (i < 11) {
-  i += 0.2;
-  if (i > 9.8 && i < 10.2) alert( i );
+function getDecimal(num) {
+	var _num = ~~num;
+	return Math.abs(Math.round((num - _num)*1000)/1000);
 }
+
+alert( getDecimal(12.345) ); // 0.345
+alert( getDecimal(1.2) ); // 0.2
+alert( getDecimal(-1.2) ); // -0.2
