79b1098ece7c663f4ccca5eac2d0d4ce6fa3a6e0
Amangeldi <Amangeldi.9606@gmail.com>
2017-11-21 18:54:05 +0500

    Усечение строки

--- alert.js
+++ alert.js
@@ -1,12 +1,12 @@
-function checkSpam(str) {
-	var str1 = "viagra", str2 = "xxx", _str = str.toLowerCase();
-	if(~_str.indexOf(str1) || ~_str.indexOf(str2))
-	{
-		return true;
+function truncate(str, maxLength) {
+	var result, _max = maxLength - 3, DOT = "...";
+	if (str.length>maxLength) {
+		result = str.slice(0, _max);
+		result = result + DOT;
+		return result;
 	}
-	return false;
+	return str;
 }
 
-alert(checkSpam('buy ViAgRA now'));
-alert(checkSpam('free xxxxx'));
-alert(checkSpam("innocent rabbit"));
+alert(truncate("Вот, что мне хотелось бы сказать на эту тему:", 20) );
+alert(truncate("Всем привет!", 20));
