d06c0d3069e0779fa2cf3326bc802c4763dcfb0c
Amangeldi <Amangeldi.9606@gmail.com>
2017-11-21 16:22:56 +0500

    рандом от 0 до max

--- alert.js
+++ alert.js
@@ -1,8 +1,2 @@
-function getDecimal(num) {
-	var _num = ~~num;
-	return Math.abs(Math.round((num - _num)*1000)/1000);
-}
-
-alert( getDecimal(12.345) ); // 0.345
-alert( getDecimal(1.2) ); // 0.2
-alert( getDecimal(-1.2) ); // -0.2
+var max = 10;
+alert( Math.random() * max );
