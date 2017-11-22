b3b7814d64d7f561d35b7c7e0e2d5fed69cfa097
Amangeldi <Amangeldi.9606@gmail.com>
2017-11-22 19:51:25 +0500

    калькулятор массива

--- alert.js
+++ alert.js
@@ -1,3 +1,17 @@
-var arr = ["Яблоко", "Апельсин", "Груша", "Лимон"];
-var rand = Math.floor(Math.random() * (arr.length));
-alert(arr[rand]);
+var numbers = [];
+
+while (true) {
+
+  var value = prompt("Введите число", 0);
+
+  if (value === "" || value === null || isNaN(value)) break;
+
+  numbers.push(+value);
+}
+
+var sum = 0;
+for (var i = 0; i < numbers.length; i++) {
+  sum += numbers[i];
+}
+
+alert( sum );
