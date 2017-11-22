c78500786a4d0537518325318ee9a740d5c6e501
Amangeldi <Amangeldi.9606@gmail.com>
2017-11-22 19:24:34 +0500

    Рандом из массива

--- alert.js
+++ alert.js
@@ -1,5 +1,3 @@
-var styles = ["Джаз", "блюз"]; alert(styles);
-styles.push("Рок-н-Ролл"); alert(styles);
-styles[styles.length-2]="Классика"; alert(styles);
-alert(styles.shift()); alert(styles);
-styles.unshift("Рэп", "Рэгги"); alert(styles);
+var arr = ["Яблоко", "Апельсин", "Груша", "Лимон"];
+var rand = Math.floor(Math.random() * (arr.length));
+alert(arr[rand]);
