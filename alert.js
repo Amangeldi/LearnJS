352de06caf97b5a9605b41e99b38369e90f16211
Amangeldi <Amangeldi.9606@gmail.com>
2017-11-22 18:53:41 +0500

    последний элемент массива

--- alert.js
+++ alert.js
@@ -1,7 +1,2 @@
-var matrix = [
-  [1, 2, 3],
-  [4, 5, 6],
-  [7, 8, 9]
-];
-
-alert( matrix[1][1] ); // центральный элемент
+var goods = ["iphone", "macbook", "x#o", "ea?", "rk"];
+alert(goods[goods.length-1]);
