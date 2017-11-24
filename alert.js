b0cb4ed63f3eb4f2a9fb2f0546f85ae0206d1df7
Amangeldi <Amangeldi.9606@gmail.com>
2017-11-24 18:26:35 +0500

    проверка существует ли аргумент

--- alert.js
+++ alert.js
@@ -1,14 +1,7 @@
-var arr = [1, 2, 3, 4, 5]
-function getSums (value)
-{
-	var red = [];
-	var result = value.reduce(function(sum, current) {
-		red.push(sum + current);
-		return sum + current;
-	}, 0);
-	return red;
+function f(x) {
+	if(arguments.length>0) alert(1);
+	else alert(0);;
 }
 
-
-alert( getSums(arr)); 
-alert(getSums([-2,-1,0,1]));
+f(undefined); // 1
+f(); // 0
