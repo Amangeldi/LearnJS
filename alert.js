ab1e30e8bdded614570bd984845caace2ecc835a
Amangeldi <Amangeldi.9606@gmail.com>
2017-11-24 17:46:20 +0500

    map

--- alert.js
+++ alert.js
@@ -1,14 +1,5 @@
-function camelize(str) {
-  var arr = str.split('-');
-
-  for (var i = 1; i < arr.length; i++) {
-    // преобразовать: первый символ с большой буквы
-    arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
-  }
-
-  return arr.join('');
-}
-
-alert( camelize("background-color") ); // backgroundColor
-alert( camelize("list-style-image") ); // listStyleImage
-alert( camelize("-webkit-transition") ); // WebkitTransition
+var arr = ["Есть", "жизнь", "на", "Марсе"];
+var arrLength = arr.map(function(f) {
+	return f.length;
+})
+alert(arrLength);
