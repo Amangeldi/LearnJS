d0475cb62f0e7d87be1e27e07c08799b4e4e65a3
Amangeldi <Amangeldi.9606@gmail.com>
2017-11-24 17:56:50 +0500

    Массив частичных сумм

--- alert.js
+++ alert.js
@@ -1,5 +1,14 @@
-var arr = ["Есть", "жизнь", "на", "Марсе"];
-var arrLength = arr.map(function(f) {
-	return f.length;
-})
-alert(arrLength);
+var arr = [1, 2, 3, 4, 5]
+function getSums (value)
+{
+	var red = [];
+	var result = value.reduce(function(sum, current) {
+		red.push(sum + current);
+		return sum + current;
+	}, 0);
+	return red;
+}
+
+
+alert( getSums(arr)); 
+alert(getSums([-2,-1,0,1]));
