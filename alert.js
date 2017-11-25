d6e8a6b3166ab7a85bc101e6a8499ab7e54f1eed
Amangeldi <Amangeldi.9606@gmail.com>
2017-11-25 19:47:26 +0500

    День указанное количество дней назад

--- alert.js
+++ alert.js
@@ -1,7 +1,10 @@
-function getLocalDay(date) {
-	var day = date.getDay();
-	if(day==0) return 7;
+function getDateAgo(date, days) {
+	var day = new Date(date);
+	day.setDate(date.getDate() - days);
 	return day;
 }
-var date = new Date(2012, 0, 3);  // 3 янв 2012
-alert( getLocalDay(date) );       // вторник, выведет 2
+var date = new Date(2015, 0, 2);
+
+alert( getDateAgo(date, 1) ); // 1, (1 января 2015)
+alert( getDateAgo(date, 2) ); // 31, (31 декабря 2014)
+alert( getDateAgo(date, 365) ); // 2, (2 января 2014)
