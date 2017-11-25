702cbca552459b5284adc9db849c9cdf010db847
Amangeldi <Amangeldi.9606@gmail.com>
2017-11-25 16:50:23 +0500

    Имя дня недели

--- alert.js
+++ alert.js
@@ -1,2 +1,13 @@
-var date = new Date(2012, 1, 20, 3, 12);
+function getWeekDay(date) {
+	var day = date.getDay();
+	if (day==1) return "Пн";
+	if (day==2) return "Вт";
+	if (day==3) return "Ср";
+	if (day==4) return "Чт";
+	if (day==5) return "Пт";
+	if (day==6) return "Сб";
+	if (day==0) return "Вс";
+}
+var date = new Date(2012, 0, 3, 3, 12);
 alert(date);
+alert( getWeekDay(date) );
