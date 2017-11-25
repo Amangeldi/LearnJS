bac4ec1e6bd555a5bca4787056f04b85fc504e0d
Amangeldi <Amangeldi.9606@gmail.com>
2017-11-25 20:28:19 +0500

    Сколько секунд до завтра

--- alert.js
+++ alert.js
@@ -1,11 +1,12 @@
 function getSecondsToday() {
-  var now = new Date();
+	var ONEDAY = 86400;
+	var now = new Date();
 
-  // создать объект из текущей даты, без часов-минут-секунд
-  var today = new Date(now.getFullYear(), now.getMonth(), now.getDate());
+	// создать объект из текущей даты, без часов-минут-секунд
+	var today = new Date(now.getFullYear(), now.getMonth(), now.getDate());
 
-  var diff = now - today; // разница в миллисекундах
-  return Math.floor(diff / 1000); // перевести в секунды
+	var diff = now - today; // разница в миллисекундах
+	return ONEDAY - Math.floor(diff / 1000); // перевести в секунды
 }
 
 alert( getSecondsToday() );
