a23a8ff151adce1d7cf8e947af633f27bf89583e
Amangeldi <Amangeldi.9606@gmail.com>
2017-11-25 16:40:46 +0500

    создать дату

--- alert.js
+++ alert.js
@@ -1,26 +1,2 @@
-var arr = [];
-for (var i = 0; i < 1000; i++) arr[i] = 0;
-
-function walkIn(arr) {
-  for (var key in arr) arr[key]++;
-}
-
-function walkLength(arr) {
-  for (var i = 0; i < arr.length; i++) arr[i]++;
-}
-
-function bench(f) {
-  for (var i = 0; i < 10000; i++) f(arr);
-}
-
-console.time("All Benchmarks");
-
-console.time("walkIn");
-bench(walkIn);
-console.timeEnd("walkIn");
-
-console.time("walkLength");
-bench(walkLength);
-console.timeEnd("walkLength");
-
-console.timeEnd("All Benchmarks");
+var date = new Date(2012, 1, 20, 3, 12);
+alert(date);
