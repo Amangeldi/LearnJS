57acc47bbdc450884c42dd364b05f463c6e66f53
Amangeldi <Amangeldi.9606@gmail.com>
2017-11-25 16:29:48 +0500

    Проверка времени исполнения функции

--- alert.js
+++ alert.js
@@ -1,7 +1,26 @@
-function f(x) {
-	if(arguments.length>0) alert(1);
-	else alert(0);;
+var arr = [];
+for (var i = 0; i < 1000; i++) arr[i] = 0;
+
+function walkIn(arr) {
+  for (var key in arr) arr[key]++;
 }
 
-f(undefined); // 1
-f(); // 0
+function walkLength(arr) {
+  for (var i = 0; i < arr.length; i++) arr[i]++;
+}
+
+function bench(f) {
+  for (var i = 0; i < 10000; i++) f(arr);
+}
+
+console.time("All Benchmarks");
+
+console.time("walkIn");
+bench(walkIn);
+console.timeEnd("walkIn");
+
+console.time("walkLength");
+bench(walkLength);
+console.timeEnd("walkLength");
+
+console.timeEnd("All Benchmarks");
