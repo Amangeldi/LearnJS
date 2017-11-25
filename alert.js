0dd78f23409b7f1616967382d42c922b298a19ae
Amangeldi <Amangeldi.9606@gmail.com>
2017-11-25 18:19:09 +0500

    День недели в европейской нумерации

--- alert.js
+++ alert.js
@@ -1,14 +1,7 @@
-function getWeekDay(date) {
+function getLocalDay(date) {
 	var day = date.getDay();
-	if (day==1) return "Пн";
-	if (day==2) return "Вт";
-	if (day==3) return "Ср";
-	if (day==4) return "Чт";
-	if (day==5) return "Пт";
-	if (day==6) return "Сб";
-	if (day==0) return "Вс";
+	if(day==0) return 7;
+	return day;
 }
-var date = new Date(2012, 0, 3, 3, 12);
-alert(date);
-alert( getWeekDay(date) );
-alert( date.toLocaleString('ru', {weekday: 'short'}) );
+var date = new Date(2012, 0, 3);  // 3 янв 2012
+alert( getLocalDay(date) );       // вторник, выведет 2
